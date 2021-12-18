de72f49dac3a0e400b1b3cd825ed73130265ef7e
Golden-Black <82011247+Golden-Black@users.noreply.github.com>
2021-12-18 16:09:02 -0600

    Add files via upload
    - All problems solved.
    - Heroku web address redirected.

--- app.js
+++ app.js
@@ -1,49 +1,69 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
-
 const app = express();
+require('dotenv').config();
+// const path = require('path');
 
 app.use(express.static("accessories"));
-
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
+    console.log("Connected")
     res.sendFile('index.html', { root: '.' });
+
+})
+
+app.get("/index.html", (req, res) => {
+    res.sendFile('index.html', { root: '.' });
+
+})
+
+app.get("/email.html", (req, res) => {
+    res.sendFile('email.html', {root: '.'});
 })
 
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 
-app.post("/", function(req, res){
-    var emailTo = req.body.email;
-    var subjectLine = req.body.subject;
-    var message = req.body.message;
+app.post("/email.html", function(req, res){
+    let emailTo = req.body.email;
+    let subjectLine = req.body.subject;
+    let message = req.body.body;
 
     const sgMail = require('@sendgrid/mail');
 
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    sgMail.setApiKey(SENDGRID_API_KEY);
+    console.log(process.env.SENDGRID_API_KEY);
 
     const msg = {
         to: emailTo,
         from: 'contact@cyberpost.xyz',
         subject: subjectLine,
-        text: message, 
+        text: message,
         // html: message,
     }
 
     sgMail
     .send(msg)
     .then(() => {
-        console.log('Email sent')
+        // res.sendFile(path.join(__dirname, + 'success.html'));
+        // res.sendFile('failure.html' + error, {root: '.'});
+        res.sendFile(__dirname + '/success.html');
+        // res.sendFile('success.html', {root: './pages'});
+        console.log('Email sent');
     })
     .catch((error) => {
-        console.error(error)
+        // res.sendFile('failure.html' + error, {root: '.'});
+        res.sendFile(__dirname + '/failure.html');
+        console.error(error);
   })
 
-    // console.log(emailTo, subjectLine, message);
+  const jsonData = JSON.stringify(msg);
+  console.log(jsonData);
+
 })
 
-app.listen(3000, function(){
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is listenning on port 3000.");
 });
